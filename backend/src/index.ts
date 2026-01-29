0fd4ca7e31ed5eb7f841295c801e89636ad73cc4
mateuszciolkowski <matitek1234@gmail.com>
2026-01-29 19:55:24 +0100

    Railway ready?

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,9 +10,29 @@ import authRouter from "./modules/auth/auth.routes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.API_PORT || 3000;
+const PORT = Number(process.env.API_PORT) || 3000;
 
-app.use(cors());
+const originsEnv = process.env.ALLOWED_ORIGINS || "localhost:5173";
+const allowedOrigins = originsEnv.split(",").map((origin) => origin.trim());
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        console.error(`CORS policy: Origin ${origin} not allowed`);
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.options("/{*splat}", cors());
+
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
@@ -20,6 +40,6 @@ app.use("/api/exercises", exerciseRouter);
 app.use("/api/users", userRouter);
 app.use("/api/workouts", workoutRouter);
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server ready at: http://localhost:${PORT}`);
 });
